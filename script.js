35f4b7634813dc2f7b8492de1b099426302dea78
Rishan-Jadva <rishanjadva@gmail.com>
2023-11-27 16:22:47 +1100

    Working basic functions

---
+++ script.js
@@ -0,0 +1,15 @@
+let firstNum;
+let operator;
+let lastNum;
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
