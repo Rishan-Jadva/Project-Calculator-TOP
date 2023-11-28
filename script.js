d4cde8b50ace474520342f2ee6744de7d3a72c68
Rishan-Jadva <rishanjadva@gmail.com>
2023-11-28 12:03:03 +1100

    All numbers/operations working except '.' and clear/delete

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let firstNum;
-let operator;
-let lastNum;
-let expression;
+let firstNum='';
+let operator='';
+let lastNum='';
+let operated = false;
 function add(a, b){
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 }
 function subtract(a, b){
     return a - b;
@@ -12,6 +12,9 @@ function multiply(a, b){
     return a * b;
 }
 function divide(a, b){
+    if (b == 0){
+        return "Error";
+    }
     return a / b;
 }
 function operate(a, b, operator){
@@ -26,18 +29,58 @@ function operate(a, b, operator){
             return divide(a,b);
     }
 }
-
 document.addEventListener('DOMContentLoaded', () =>{
     numberButton = document.getElementsByClassName('number');
     Array.from(numberButton).forEach(element => {
     element.addEventListener('click' ,(event)=>{
-        updateScreen(event.target.textContent);
+        if(operated === true){
+            screenText = document.getElementById('screen')
+            updateNumber(event.target.textContent);
+            operated = false;
+        }
+        else{
+            updateNumber(event.target.textContent);
+        }
     })
 });
+    operatorButton = document.getElementsByClassName('operator');
+    Array.from(operatorButton).forEach(element => {
+    element.addEventListener('click' ,(event)=>{
+        if(operator.length == 1){
+            equals();
+        }
+        updateOperator(event.target.textContent);
+    })
+});
+    equalButton = document.getElementsByClassName('equal');
+    Array.from(equalButton).forEach(element => {
+    element.addEventListener('click' ,equals)
+});
+function equals(){
+    screenText = document.getElementById("screen");
+    screenText.textContent = parseFloat(operate(firstNum, lastNum, operator).toFixed(3)).toString();
+    firstNum = screenText.textContent;
+    operator = '';
+    lastNum = '';
+    operated = true;
+}
 })
 
-function updateScreen (text){
-    screenText = document.getElementById("screen");
+function updateNumber (text){
+    if(operator.length >=1){
+        updateNumberTwo(text);
+    } 
+    else{
+        screenText = document.getElementById("screen");
+        screenText.textContent += text;
+        firstNum = screenText.textContent;
+    }
+}
+function updateNumberTwo (text){
     screenText.textContent += text;
-    expression = screenText.textContent;
+    lastNum += text;
+}
+function updateOperator (text){
+    screenText.textContent += text;
+    operator = text;
 }
